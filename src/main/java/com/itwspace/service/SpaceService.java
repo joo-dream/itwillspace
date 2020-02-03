309b1fe1ef32c23db874d2237486d1bef97c75e6
best-hub <56568534+best-hub@users.noreply.github.com>
2020-02-03 19:08:20 +0900

    Update SpaceService.java
    29 라인 밑 추가

--- src/main/java/com/itwspace/service/SpaceService.java
+++ src/main/java/com/itwspace/service/SpaceService.java
@@ -25,4 +25,11 @@ public interface SpaceService {
 	public void write(SpaceVO vo) throws Exception;
 
 	public SpaceVO view(int id) throws Exception;
+	
+	public List<SpaceAttachVO> getAttachList(String space_id) throws Exception;
+
+	boolean remove(String space_id) throws Exception;
+	
+	public List<SpaceVO> hostPage(int startPost, int countList) throws Exception;
+
 }
