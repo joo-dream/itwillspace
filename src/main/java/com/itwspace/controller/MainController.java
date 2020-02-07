a17a70366967c3e55761b0f58c89ea33f1d2289f
henini011 <qlrqod1110@gmail.com>
2020-02-07 10:33:11 +0900

    메인 폼 1차 수정 완료( MainController _ space 디비 연결 완료 _ 사진제외  / index.jsp / style.css 수정)

--- src/main/java/com/itwspace/controller/MainController.java
+++ src/main/java/com/itwspace/controller/MainController.java
@@ -1,15 +1,32 @@
 package com.itwspace.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itwspace.model.SpaceVO;
+import com.itwspace.service.SpaceService;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 @Controller
 public class MainController {
 
-	@RequestMapping("/")
-	public String main(Model model) {
-		return "index";
-	}
+   @Autowired
+   private SpaceService  sService;
+   
+   @RequestMapping("/")
+   public String main(Model model) {
+      
+      List<SpaceVO> vo = sService.list();
+      model.addAttribute("spaceVO", vo);
+      log.info(">>>>> bookingConfirm으로 끌고 갈 정보(hidden)" + sService.list());
+      
+      return "index";
+   }
 }
